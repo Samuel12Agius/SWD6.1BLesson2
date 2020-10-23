5644a0cc1dda442f7f17f48ce9b783fa055530cc
Samuel Agius <Samuel.Agius.d57031@mcast.edu.mt>
2020-10-23 10:55:36 +0200

    Lesson 1 - Introduction to OOP (classes ,fields,properties,methods)

---
+++ Oop/Lesson1_IntroToOop/Person.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_IntroToOop
+{
+    //class: is a blueprint (template) that defines an entity structure. An entity (usually)represents a real-life model
+    //       object which is described using chracteristics.
+    //      :use Pascal casing for class names.
+    //Field: is a variable which describes something about the model/object
+        //: use camel casing for field names.
+    //property: is (usually) declared as public and controls the accessiblity to the field
+    class Person //an example of a class
+    {
+        string idCardNo;
+        private string name; //an example of a field
+        string surname;
+        int age;
+        DateTime dateOfBirth;
+
+      //using this method or under Surname String..  string address;
+        string mobileNo;
+        string postCode;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string IdCard
+        {
+            get { return idCardNo; }
+            set
+            {
+                switch (value.ToString().Length)
+                {
+                    case 5:
+                        idCardNo = "00" + value;
+                        break;
+                    case 6:
+                        idCardNo = "0" + value;
+                        break;
+                    default:
+                        idCardNo = value.ToString();
+                        break;
+                }
+                idCardNo = value.ToString().ToUpper();
+            }
+        }
+
+        public string FullName //read only property
+        {
+            get { return Name + " " + Surname; }
+        }
+
+        public string Surname { get => surname; set => surname = value; }
+
+
+        public string Address { get; set; } 
+        //Taking a shortcut (if we know that we do not need any particular implementation for this particular member.
+        //public string MobileNo { get; set; }
+        //public string postCode { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int CalculateAge()
+        {
+            return DateTime.Today.Year - DateOfBirth.Year;
+        }
+
+        public string DelimitAddress(char c)
+        {
+            string[] delimitedAddress = Address.Split(new char[] { ' ' });
+            string output = "";
+            foreach(string s in delimitedAddress)
+            {
+                output += s + c + " ";
+            }
+            return output.Remove(output.Length-1);
+        }
+    }
+}
---
+++ Oop/Lesson1_IntroToOop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_IntroToOop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.IdCard = "3838383m"; //write
+
+            //Console.WriteLine(p.IdCard); //read
+
+            p.Name = "Ryan";
+            p.Surname = "Attard";
+            /*
+            Console.WriteLine("Please press a key to continue");
+            Console.ReadLine(); // This expects user clicks on the Enter key on the keyboard
+
+            Console.WriteLine($"Name: {p.Name}, Surname: {p.Surname}, Id: {p.IdCard}");
+
+            Console.ReadKey();// This expects the user clicks on any key.
+            */
+
+            Console.WriteLine("Please input Address:");
+            p.Address = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(p.DelimitAddress(','));
+            Console.WriteLine("Press a key to terminate app...");
+            Console.ReadKey();
+      
+
+
+        }
+    }
+}
